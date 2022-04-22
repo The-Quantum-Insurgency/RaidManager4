f164e131c718a03ae8214e1fac39ee78088b441f
imskyyc <santafejayyt@gmail.com>
2022-04-22 00:29:03 -0700

    sync

--- bot/events/ready.js
+++ bot/events/ready.js
@@ -5,9 +5,26 @@
  * @author imskyyc
  * @param { Bot } Bot
  */
+const FileSystem = require("fs/promises");
 
 module.exports = async function (Bot) {
-  console.log('ready');
+  // Check for raidmanager tempfile
+  try {
+    await FileSystem.stat("raidmanager.temp");
+
+    const Tempdata = await (
+      await FileSystem.readFile("raidmanager.temp")
+    ).toString();
+
+    const ShutdownData = Tempdata.split("\n");
+    const ShutdownChannel = ShutdownData[0];
+    const ShutdownMessage = ShutdownData[1];
+  } catch (err) {
+    if (err.code != "ENOENT") {
+      console.error(`Error: ${err.path}: ${err.code} @ ${err.syscall}`);
+    }
+  }
+
   // Send slash commands to discord API
   const Commands = Bot.Commands;
   const CommandArray = await Bot.CommandCollectionToArray(Commands);
